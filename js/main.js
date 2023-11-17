de8db24431327b1afab4afeb48b60ac42c51774c
vinhthanh89 <vinhthanh89@gmail.com>
2023-11-17 20:39:30 +0700

    fix submitLogin function

--- js/main.js
+++ js/main.js
@@ -2,24 +2,26 @@ let brandDropDown = document.getElementById('brand-filter-list');
 let priceDropDown = document.getElementById('price-filter-list')
 let sizeDropDown = document.getElementById('size-filter-list')
 let display = 0;
-function hideshow(para){
-    if (display == 1){
+function hideshow(para) {
+    if (display == 1) {
         para.style.display = 'block'
         display = 0;
-    }else{
+    } else {
         para.style.display = 'none';
         display = 1;
     }
 }
 //đổi tham số para thành các biến element cần đóng mở
 
-let loginUsername = String(document.getElementById('loginUsername').value);
-let loginPassword = String(document.getElementById('loginPassword').value);
 
-function submitLogin(){
-    if (loginUsername == "vinhthanh" && loginPassword == "123456"){
-        alert("Bạn đã đăng nhập thành công")
-    }else{
+
+function submitLogin() {
+    const loginUsername = String(document.getElementById('loginUsername').value);
+    const loginPassword = String(document.getElementById('loginPassword').value);
+    console.log(loginPassword);
+    if (loginUsername == "vinhthanh" && loginPassword == "123456") {
+        window.location.href = "index.html"
+    } else {
         alert("Xin đăng nhập lại username và password chính xác")
     }
 }
