1cc053386d4727bffdbb8150561a5e984f8d9532
vinhthanh89 <vinhthanh89@gmail.com>
2023-11-17 19:57:28 +0700

    frist commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let brandDropDown = document.getElementById('brand-filter-list');
+let priceDropDown = document.getElementById('price-filter-list')
+let sizeDropDown = document.getElementById('size-filter-list')
+let display = 0;
+function hideshow(para){
+    if (display == 1){
+        para.style.display = 'block'
+        display = 0;
+    }else{
+        para.style.display = 'none';
+        display = 1;
+    }
+}
+//đổi tham số para thành các biến element cần đóng mở
+
+let loginUsername = String(document.getElementById('login-username').value);
+let loginPassword = String(document.getElementById('login-password').value);
+
+function submitLogin(){
+    if (loginUsername === "username" && loginPassword === "123456"){
+        window.location.href = "index.html"
+    }else{
+        alert("Xin đăng nhập lại username và password chính xác")
+    }
+}
+
+
