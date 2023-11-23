7f8f44d89ba9823212a71765d91cf692d585e078
vinhthanh89 <vinhthanh89@gmail.com>
2023-11-23 20:40:26 +0700

    add slick-slider to login page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-$('.slider').slick({
-    dots: true,
-    arrows: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    fade: true,
-    slidesToShow: 1,
-    prevArrow:
-        "<button type='button' class='slick-prev pull-left'><i class='bi bi-chevron-double-left'></i></button>",
-    nextArrow:
-        "<button type='button' class='slick-next pull-right'><i class='bi bi-chevron-double-right'></i></button>"
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        fade: true,
+        slidesToShow: 1,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><i class='bi bi-chevron-double-left'></i></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><i class='bi bi-chevron-double-right'></i></button>"
+    });
 });
 
 let brandDropDown = document.getElementById('brand-filter-list');
@@ -26,28 +28,28 @@ function hideshow(para) {
     }
 }
 
-function loginFunction() {
-    const loginUsername = String(document.getElementById('loginUsername').value);
-    const loginPassword = String(document.getElementById('loginPassword').value);
-    if (loginUsername == "vinhthanh" && loginPassword == "123456") {
-        window.location.href = "https://vinhthanh89.github.io/Final_Project/"
-    } else {
-        alert("Xin đăng nhập lại username và password chính xác")
-    }
-}
-
-
-// document.querySelector(".login-form").addEventListener("submit",
-//     function () {
-//         e.preventDefault()
-//         console.log(e);
-//         const loginUsername = String(document.getElementById('loginUsername').value);
-//         const loginPassword = String(document.getElementById('loginPassword').value);
-//         if (loginUsername == "vinhthanh" && loginPassword == "123456") {
-//             window.location.href  = "https://vinhthanh89.github.io/Final_Project/"
-//         } else {
-//             alert("Xin đăng nhập lại username và password chính xác")
-//         }
+// function loginFunction() {
+//     const loginUsername = String(document.getElementById('loginUsername').value);
+//     const loginPassword = String(document.getElementById('loginPassword').value);
+//     if (loginUsername == "vinhthanh" && loginPassword == "123456") {
+//         window.location.href = "https://vinhthanh89.github.io/Final_Project/"
+//     } else {
+//         alert("Xin đăng nhập lại username và password chính xác")
 //     }
-// )
+// }
+
+
+document.querySelector(".login-form").addEventListener("submit",
+    function (e) {
+        e.preventDefault(e)
+        console.log(e);
+        const loginUsername = String(document.getElementById('loginUsername').value);
+        const loginPassword = String(document.getElementById('loginPassword').value);
+        if (loginUsername === "vinhthanh" && loginPassword === "123456") {
+            window.location.href = "https://vinhthanh89.github.io/Final_Project/"
+        } else {
+            alert("Xin đăng nhập lại username và password chính xác")
+        }
+    }
+)
 
